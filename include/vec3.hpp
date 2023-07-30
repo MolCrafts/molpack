041a6210eb3102a907fafcdbe58fdd93cba9c87d
KUNzfw <zhuofengwang2003@gmail.com>
2023-07-30 20:26:37 +0800

    Fix warning under mvsc and add .clang-format file.

--- include/vec3.hpp
+++ include/vec3.hpp
@@ -15,33 +15,46 @@ struct is_arithmetic_vec3<Vec3<T>> : std::is_arithmetic<T> {};
 
 template <typename T>
 class Vec3 {
- private:
+private:
   T inner_[3]{};
 
- public:
+public:
   Vec3<T>() = default;
   Vec3<T>(const T& x, const T& y, const T& z) : inner_{x, y, z} {}
 
-  inline const T& getX() const { return inner_[0]; }
-  inline const T& getY() const { return inner_[1]; }
-  inline const T& getZ() const { return inner_[2]; }
+  inline const T& getX() const {
+    return inner_[0];
+  }
+  inline const T& getY() const {
+    return inner_[1];
+  }
+  inline const T& getZ() const {
+    return inner_[2];
+  }
 
-  inline void setX(T new_val) { inner_[0] = new_val; }
-  inline void setY(T new_val) { inner_[1] = new_val; }
-  inline void setZ(T new_val) { inner_[2] = new_val; }
+  inline void setX(T new_val) {
+    inner_[0] = new_val;
+  }
+  inline void setY(T new_val) {
+    inner_[1] = new_val;
+  }
+  inline void setZ(T new_val) {
+    inner_[2] = new_val;
+  }
 
-  const T& operator[](std::size_t index) const { return inner_[index]; }
-  T& operator[](std::size_t index) { return inner_[index]; }
+  inline const T& operator[](std::size_t index) const {
+    return inner_[index];
+  }
+  inline T& operator[](std::size_t index) {
+    return inner_[index];
+  }
 
   template <typename U, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
   inline friend auto operator+(const Vec3<T>& lhs, const U& rhs) {
     if constexpr (std::is_arithmetic_v<U>) {
-      return Vec3<decltype(T{} + U{})>(lhs.getX() + rhs, lhs.getY() + rhs,
-                                       lhs.getZ() + rhs);
+      return Vec3<decltype(T{} + U{})>(lhs[0] + rhs, lhs[1] + rhs, lhs[2] + rhs);
     } else if constexpr (is_arithmetic_vec3<U>::value) {
-      return Vec3<decltype(T{} + U{}[0])>(lhs.getX() + rhs.getX(),
-                                          lhs.getY() + rhs.getY(),
-                                          lhs.getZ() + rhs.getZ());
+      return Vec3<decltype(T{} + U{}[0])>(lhs[0] + rhs[0], lhs[1] + rhs[1], lhs[2] + rhs[2]);
     } else {
       // std::is_arithmetic_v<U> always false, use this to avoid clangd error
       static_assert(std::is_arithmetic_v<U>,
@@ -58,12 +71,9 @@ class Vec3 {
   template <typename U, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
   inline friend auto operator-(const Vec3<T>& lhs, const U& rhs) {
     if constexpr (std::is_arithmetic_v<U>) {
-      return Vec3<decltype(T{} - U{})>(lhs.getX() - rhs, lhs.getY() - rhs,
-                                       lhs.getZ() - rhs);
+      return Vec3<decltype(T{} - U{})>(lhs[0] - rhs, lhs[1] - rhs, lhs[2] - rhs);
     } else if constexpr (is_arithmetic_vec3<U>::value) {
-      return Vec3<decltype(T{} - U{}[0])>(lhs.getX() - rhs.getX(),
-                                          lhs.getY() - rhs.getY(),
-                                          lhs.getZ() - rhs.getZ());
+      return Vec3<decltype(T{} - U{}[0])>(lhs[0] - rhs[0], lhs[1] - rhs[1], lhs[2] - rhs[2]);
     } else {
       // std::is_arithmetic_v<U> always false, use this to avoid clangd error
       static_assert(std::is_arithmetic_v<U>,
@@ -79,19 +89,15 @@ class Vec3 {
   template <typename U, typename = std::enable_if_t<std::is_arithmetic_v<U>>,
             typename = std::enable_if_t<std::is_arithmetic_v<T>>>
   inline friend auto operator-(const U& lhs, const Vec3<T>& rhs) {
-    return Vec3<decltype(U{} - T{})>{lhs - rhs.getX(), lhs - rhs.getY(),
-                                     lhs - rhs.getZ()};
+    return Vec3<decltype(U{} - T{})>{lhs - rhs[0], lhs - rhs[1], lhs - rhs[2]};
   }
 
   template <typename U>
   inline friend auto operator*(const Vec3<T>& lhs, const U& rhs) {
     if constexpr (std::is_arithmetic_v<U>) {
-      return Vec3<decltype(T{} * U{})>(lhs.getX() * rhs, lhs.getY() * rhs,
-                                       lhs.getZ() * rhs);
+      return Vec3<decltype(T{} * U{})>(lhs[0] * rhs, lhs[1] * rhs, lhs[2] * rhs);
     } else if constexpr (is_arithmetic_vec3<U>::value) {
-      return Vec3<decltype(T{} * U{}[0])>(lhs.getX() * rhs.getX(),
-                                          lhs.getY() * rhs.getY(),
-                                          lhs.getZ() * rhs.getZ());
+      return Vec3<decltype(T{} * U{}[0])>(lhs[0] * rhs[0], lhs[1] * rhs[1], lhs[2] * rhs[2]);
     } else {
       // std::is_arithmetic_v<U> always false, use this to avoid clangd error
       static_assert(std::is_arithmetic_v<U>,
@@ -108,12 +114,11 @@ class Vec3 {
   template <typename U>
   inline friend auto operator/(const Vec3<T>& lhs, const U& rhs) {
     if constexpr (std::is_arithmetic_v<U>) {
-      return Vec3<decltype(T{} / U{})>(lhs.getX() / rhs, lhs.getY() / rhs,
-                                       lhs.getZ() / rhs);
+      return Vec3<decltype(T{} / U{})>(lhs.inner_[0] / rhs, lhs.inner_[1] / rhs,
+                                       lhs.inner_[2] / rhs);
     } else if constexpr (is_arithmetic_vec3<U>::value) {
-      return Vec3<decltype(T{} / U{}[0])>(lhs.getX() / rhs.getX(),
-                                          lhs.getY() / rhs.getY(),
-                                          lhs.getZ() / rhs.getZ());
+      return Vec3<decltype(T{} / U{}[0])>(lhs.inner_[0] / rhs[0], lhs.inner_[1] / rhs[1],
+                                          lhs.inner_[2] / rhs[2]);
     } else {
       // std::is_arithmetic_v<U> always false, use this to avoid clangd error
       static_assert(std::is_arithmetic_v<U>,
@@ -124,20 +129,19 @@ class Vec3 {
   template <typename U, typename = std::enable_if_t<std::is_arithmetic_v<U>>,
             typename = std::enable_if_t<std::is_arithmetic_v<T>>>
   inline friend auto operator/(const U& lhs, const Vec3<T>& rhs) {
-    return Vec3<decltype(U{} / T{})>{lhs / rhs.getX(), lhs / rhs.getY(),
-                                     lhs / rhs.getZ()};
+    return Vec3<decltype(U{} / T{})>{lhs / rhs[0], lhs / rhs[1], lhs / rhs[2]};
   }
 
   template <typename U, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
   inline Vec3<T>& operator+=(const U& rhs) {
     if constexpr (std::is_arithmetic_v<U>) {
-      inner_[0] += rhs;
-      inner_[1] += rhs;
-      inner_[2] += rhs;
+      inner_[0] = static_cast<T>(inner_[0] + rhs);
+      inner_[1] = static_cast<T>(inner_[1] + rhs);
+      inner_[2] = static_cast<T>(inner_[2] + rhs);
     } else if constexpr (is_arithmetic_vec3<U>::value) {
-      inner_[0] += rhs.getX();
-      inner_[1] += rhs.getY();
-      inner_[2] += rhs.getZ();
+      inner_[0] = static_cast<T>(inner_[0] + rhs[0]);
+      inner_[1] = static_cast<T>(inner_[1] + rhs[1]);
+      inner_[2] = static_cast<T>(inner_[2] + rhs[2]);
     } else {
       // std::is_arithmetic_v<U> always false, use this to avoid clangd error
       static_assert(std::is_arithmetic_v<U>,
@@ -149,13 +153,13 @@ class Vec3 {
   template <typename U, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
   inline Vec3<T>& operator-=(const U& rhs) {
     if constexpr (std::is_arithmetic_v<U>) {
-      inner_[0] -= rhs;
-      inner_[1] -= rhs;
-      inner_[2] -= rhs;
+      inner_[0] = static_cast<T>(inner_[0] - rhs);
+      inner_[1] = static_cast<T>(inner_[1] - rhs);
+      inner_[2] = static_cast<T>(inner_[2] - rhs);
     } else if constexpr (is_arithmetic_vec3<U>::value) {
-      inner_[0] -= rhs.getX();
-      inner_[1] -= rhs.getY();
-      inner_[2] -= rhs.getZ();
+      inner_[0] = static_cast<T>(inner_[0] - rhs[0]);
+      inner_[1] = static_cast<T>(inner_[1] - rhs[1]);
+      inner_[2] = static_cast<T>(inner_[2] - rhs[2]);
     } else {
       // std::is_arithmetic_v<U> always false, use this to avoid clangd error
       static_assert(std::is_arithmetic_v<U>,
@@ -167,13 +171,13 @@ class Vec3 {
   template <typename U, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
   inline Vec3<T>& operator*=(const U& rhs) {
     if constexpr (std::is_arithmetic_v<U>) {
-      inner_[0] *= rhs;
-      inner_[1] *= rhs;
-      inner_[2] *= rhs;
+      inner_[0] = static_cast<T>(inner_[0] * rhs);
+      inner_[1] = static_cast<T>(inner_[1] * rhs);
+      inner_[2] = static_cast<T>(inner_[2] * rhs);
     } else if constexpr (is_arithmetic_vec3<U>::value) {
-      inner_[0] *= rhs.getX();
-      inner_[1] *= rhs.getY();
-      inner_[2] *= rhs.getZ();
+      inner_[0] = static_cast<T>(inner_[0] * rhs[0]);
+      inner_[1] = static_cast<T>(inner_[1] * rhs[1]);
+      inner_[2] = static_cast<T>(inner_[2] * rhs[2]);
     } else {
       // std::is_arithmetic_v<U> always false, use this to avoid clangd error
       static_assert(std::is_arithmetic_v<U>,
@@ -185,13 +189,13 @@ class Vec3 {
   template <typename U, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
   inline Vec3<T>& operator/=(const U& rhs) {
     if constexpr (std::is_arithmetic_v<U>) {
-      inner_[0] /= rhs;
-      inner_[1] /= rhs;
-      inner_[2] /= rhs;
+      inner_[0] = static_cast<T>(inner_[0] / rhs);
+      inner_[1] = static_cast<T>(inner_[1] / rhs);
+      inner_[2] = static_cast<T>(inner_[2] / rhs);
     } else if constexpr (is_arithmetic_vec3<U>::value) {
-      inner_[0] /= rhs.getX();
-      inner_[1] /= rhs.getY();
-      inner_[2] /= rhs.getZ();
+      inner_[0] = static_cast<T>(inner_[0] / rhs[0]);
+      inner_[1] = static_cast<T>(inner_[1] / rhs[1]);
+      inner_[2] = static_cast<T>(inner_[2] / rhs[2]);
     } else {
       // std::is_arithmetic_v<U> always false, use this to avoid clangd error
       static_assert(std::is_arithmetic_v<U>,
@@ -202,8 +206,7 @@ class Vec3 {
 
   template <typename = std::enable_if_t<std::is_arithmetic_v<T>>>
   inline double norm() const {
-    return std::sqrt(inner_[0] * inner_[0] + inner_[1] * inner_[1] +
-                     inner_[2] * inner_[2]);
+    return std::sqrt(inner_[0] * inner_[0] + inner_[1] * inner_[1] + inner_[2] * inner_[2]);
   }
 
   template <typename = std::enable_if_t<std::is_arithmetic_v<T>>>
